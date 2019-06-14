b67b6afd7668381734b1e7b93e665d571b95ee91
gioung <ska2253@naver.com>
2019-06-14 21:00:22 +0900

    initial commit

--- prob01.py
+++ prob01.py
@@ -1,10 +1,6 @@
 # 파이썬 경로명 s = '/usr/local/bin/python' 에서 각각의 디렉토리 경로명을 분리하여 출력하세요
 s = '/usr/local/bin/python'
-list = s.split('/')
+list = s[1:].split('/')
 list2 = s.rsplit('/', 1)
-if list.count('') !=0:
-    list.remove('')
-
-
 print(list)
 print(list2)
